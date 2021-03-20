38ec17cc2cbd8db8b174e8d4a6404bbf2013f75c
Leonardo Eiji Koshimura <leonardo.koshimura@gmail.com>
2021-03-20 01:01:43 -0300

    feat: load books with BooksAPI
    The collection of books and its bookshelves are aquired from
    the proveded BooksAPI succesfully.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import { Route, Link } from "react-router-dom";
 
 class BooksApp extends React.Component {
+  state = {
+    books: null
+  };
+
+  componentDidMount = () => {
+    BooksAPI.getAll()
+      .then(books => this.setState({books: books}))
+  };
+
   render() {
     return (
       <div className="app">
