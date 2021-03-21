b618934db41a728da6f23f106ff7817ad5f97cad
Leonardo Eiji Koshimura <leonardo.koshimura@gmail.com>
2021-03-21 00:55:59 -0300

    Search component: books that match the query are displayed on the page

---
+++ src/SearchBooks.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import * as BooksAPI from './BooksAPI';
+import Book from './Book';
+
+class SearchBooks extends React.Component {
+  state = {
+    searchResult: []
+  };
+
+  onSearchChange = (event) => {
+    const searchTerm = event.target.value;
+    if (searchTerm.length === 0) {
+      this.setState({searchResult: []});
+    } else {
+      BooksAPI.search(searchTerm)
+        .then(data => this.setState({searchResult: data}))
+    };
+  };
+
+  render() {
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link
+            className="close-search"
+            to='/'>Close</Link>
+          <div className="search-books-input-wrapper">
+            {/*
+            NOTES: The search from BooksAPI is limited to a particular set of search terms.
+            You can find these search terms here:
+            https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+            However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+            you don't find a specific author or title. Every search is limited by search terms.
+            */}
+            <input type="text" placeholder="Search by title or author" onChange={this.onSearchChange} />
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid">
+            {this.state.searchResult && this.state.searchResult.map(
+              book => (
+                <li key={book.id}>
+                  <Book
+                    book={book}
+                  />
+                </li>
+              )
+            )}
+          </ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default SearchBooks;
